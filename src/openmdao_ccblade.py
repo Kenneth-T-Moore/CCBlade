Fixed precone and precurve gradients

src/openmdao_ccblade.py
@@ -184,9 +184,9 @@ class CCInit(Component):
 
     def solve_nonlinear(self, params, unknowns, resids):
 
-        precone = np.radians(params['precone'])
-        tilt = np.radians(params['tilt'])
-        yaw = np.radians(params['yaw'])
+        precone = params['precone']
+        tilt = params['tilt']
+        yaw = params['yaw']
         shearExp = params['shearExp']
         Rtip = params['Rtip']
         precurveTip = params['precurveTip']
@@ -211,7 +211,7 @@ class CCInit(Component):
 
         J = {}
 
-        precone = np.radians(params['precone'])
+        precone = params['precone']
         precurveTip = params['precurveTip']
         Rtip = params['Rtip']
         tsr = params['tsr']
@@ -301,8 +301,8 @@ class WindComponents(Component):
         dVx_dr = np.diag(Vxd[:n, :])  # off-diagonal terms are known to be zero and not needed
         dVy_dr = np.diag(Vyd[:n, :])
 
-        dVx_dcurve = Vxd[n:2*n, :]  # tri-diagonal  (note: dVx_j / dcurve_i  i==row)
-        dVy_dcurve = Vyd[n:2*n, :]  # off-diagonal are actually all zero, but leave for convenience
+        dVx_dcurve = Vxd[n:2*n, :].T  # tri-diagonal  (note: dVx_j / dcurve_i  i==row)
+        dVy_dcurve = Vyd[n:2*n, :].T  # off-diagonal are actually all zero, but leave for convenience
 
         dVx_dsweep = np.diag(Vxd[2*n:3*n, :])  # off-diagonal terms are known to be zero and not needed
         dVy_dsweep = np.diag(Vyd[2*n:3*n, :])
@@ -1541,19 +1541,19 @@ if __name__ == "__main__":
     ccblade['Rtip'] = Rtip
     ccblade['r'] = r
     ccblade['chord'] = chord
-    ccblade['theta'] = theta
+    ccblade['theta'] = np.radians(theta)
     ccblade['B'] = B
     ccblade['rho'] = rho
     ccblade['mu'] = mu
-    ccblade['tilt'] = tilt
-    ccblade['precone'] = precone
-    ccblade['yaw'] = yaw
+    ccblade['tilt'] = np.radians(tilt)
+    ccblade['precone'] = np.radians(precone)
+    ccblade['yaw'] = np.radians(yaw)
     ccblade['shearExp'] = shearExp
     ccblade['hubHt'] = hubHt
     ccblade['nSector'] = nSector
     ccblade['Uinf'] = Uinf
     ccblade['tsr'] = tsr
-    ccblade['pitch'] = pitch
+    ccblade['pitch'] = np.radians(pitch)
 
     ccblade.run()
 

test/test_gradients.py
@@ -21,20 +21,10 @@ from openmdao.api import Problem
 from openmdao_ccblade import CCAirfoil, CCBlade, LoadsGroup
 
 class TestGradientsClass(unittest.TestCase):
-
-    # def __init__(self, *args, **kwargs):
-    #     super(TestGradients, self).__init__(*args, **kwargs)
     @classmethod
     def setUpClass(cls):
        pass
 
-    # @staticmethod
-    # def disconnect():
-    #     TestGradientsClass.disconnect()
-    #
-    # @classmethod
-    # def tearDownClass(cls):
-    #     cls.disconnect()
 
 class TestGradients(TestGradientsClass):
 
@@ -123,7 +113,7 @@ class TestGradients(TestGradientsClass):
         loads.run()
         loads_test_total_gradients = open('loads_test_total_gradients.txt', 'w')
         loads_gradients = loads.check_total_derivatives_modified(out_stream=loads_test_total_gradients)
-        loads_partials = loads.check_partial_derivatives(out_stream=loads_test_total_gradients)
+        # loads_partials = loads.check_partial_derivatives(out_stream=loads_test_total_gradients)
         ## Power Gradients
         bemoptions = dict(usecd=True, tiploss=True, hubloss=True, wakerotation=True)
         ccblade = Problem()
@@ -148,11 +138,11 @@ class TestGradients(TestGradientsClass):
         ccblade['pitch'] = np.radians(pitch)
 
         ccblade.run()
-        print "Generating gradients. Please wait..."
+        print "Generating gradients for Test 1. Please wait..."
         power_test_total_gradients = open('power_test_total_gradients.txt', 'w')
         power_gradients = ccblade.check_total_derivatives_modified2(out_stream=power_test_total_gradients)
-        power_partial = ccblade.check_partial_derivatives(out_stream=power_test_total_gradients)
-        print "Gradients generated."
+        # power_partial = ccblade.check_partial_derivatives(out_stream=power_test_total_gradients)
+        print "Gradients generated for Test 1."
         self.loads_gradients = loads_gradients
         self.power_gradients = power_gradients
         self.n = len(r)
@@ -1056,8 +1046,9 @@ class TestGradientsNotRotating(TestGradientsClass):
 
         loads.run()
         loads_test_total_gradients = open('loads_test_total_gradients.txt', 'w')
+        print "Generating gradients for Test 2. Please wait."
         loads_gradients = loads.check_total_derivatives_modified(out_stream=loads_test_total_gradients)
-
+        print "Gradients generated for Test 2."
         cls.loads_gradients = loads_gradients
         cls.n = len(r)
         cls.npts = 1  # len(Uinf)
@@ -1384,13 +1375,12 @@ class TestGradientsFreestreamArray(unittest.TestCase):
             ccblade.run()
 
             power_test_total_gradients = open('power_test_total_gradients.txt', 'w')
-            print "Calculating gradients. Please wait..."
+            print "Generating gradients for Test " + str(i+3) + ". Please wait..."
             power_gradients_sub = ccblade.check_total_derivatives_modified2(out_stream=power_test_total_gradients)
-            print "Gradients " + str(i+1) + " calculated."
+            print "Gradients " + str(i+3) + " calculated."
             power_gradients[i] = power_gradients_sub
 
         cls.power_gradients = power_gradients
-
         cls.n = len(r)
         cls.npts = len(Uinf)
 